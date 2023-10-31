26844cf28ca3e6356dfb2a957ce78cb447d9421a
Ankit Mishra <dezinerankit@gmail.com>
2023-10-31 22:16:16 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='italic'>Hey I m learning here</p>
+    </div>
+  );
+}
+
+export default App;
